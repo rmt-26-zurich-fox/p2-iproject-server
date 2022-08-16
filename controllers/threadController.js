2fa60013e1535314fc00dcb8d8adce566b5ea156
erlanggakpp <erlanggakencana@gmail.com>
2022-08-16 11:33:41 +0700

    Added create thread feature

--- controllers/threadController.js
+++ controllers/threadController.js
@@ -1,5 +1,27 @@
 const { Thread } = require("../models");
+const router = require("../routes");
 
-class ThreadController {}
+class ThreadController {
+  static async createThread(req, res, next) {
+    try {
+      const { title, description, explicit, ProfileId, closed, CategoryId } =
+        req.body;
+      const { profileId } = req.user;
+      const createdThread = await Thread.create({
+        title,
+        description,
+        explicit,
+        ProfileId: profileId,
+        closed: false,
+        CategoryId,
+      });
+      res
+        .status(201)
+        .json({ message: `Created new thread with ID ${createdThread.id}` });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
 
 module.exports = ThreadController;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const UserRouter = require("../routes/userRouter");
 const ProfileRouter = require("../routes/profileRouter");
+const ThreadRouter = require("../routes/threadRouter");
 const errorHandler = require("../middlewares/errorHandler");
 const { authentication } = require("../middlewares/authentication");
 const router = require("express").Router();
 
 router.use("/users", UserRouter);
 router.use("/profiles", authentication, ProfileRouter);
+router.use("/threads", ThreadRouter);
 
 router.use(errorHandler);
 module.exports = router;
