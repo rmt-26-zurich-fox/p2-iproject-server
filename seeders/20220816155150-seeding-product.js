4af473bec832010f21d9f41e52d06a2e0a1784f2
Rio Hanantyoko <rhanantyoko@gmail.com>
2022-08-16 23:13:00 +0700

    database created

---
+++ seeders/20220816155150-seeding-product.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const data = require('../MOCK_DATA_thesouthface.json')
+
+data.forEach(el=>[
+  el.createdAt = el.updatedAt = new Date()
+])
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Products', data, {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('Products', null, {})
+  }
+};
