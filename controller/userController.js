18e6d5411c34f61b27dd94f1e84f38999ac8d094
Ghozi Fattah Allauddin <ghozifattah@gmail.com>
2022-08-16 14:31:46 +0700

    feat: register users done

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,36 @@
+const { User } = require("../models");
+
 class UserController {
 
+    static async registerAdmin(req, res, next) {
+        try {
+            const { username, email, password } = req.body;
+            const registerAdmin = await User.create({ username, email, password, role: "Admin" });
+            res.status(201).json({
+                message: "Success create new Admin",
+                id: registerAdmin.id,
+                email : registerAdmin.email
+            })
+        } catch (error) {
+            console.log(error);
+            next(error);
+        }
+    } 
+    
+    static async registerVisitor(req, res, next) {
+        try {
+            const { username, email, password } = req.body;
+            const registerVisitor = await User.create({ username, email, password, role: "Visitor" });
+            res.status(201).json({
+                message: "Success create new Visitor",
+                id: registerVisitor.id,
+                email : registerVisitor.email
+            })
+        } catch (error) {
+            next(error);
+        }
+    } 
+    
 }
 
 module.exports = UserController;
