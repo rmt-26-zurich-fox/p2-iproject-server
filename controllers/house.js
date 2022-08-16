d1de65075f2b5705d77df0eb1fecd7164fe4ab9e
rysaldiwijaya <rysaldii@gmail.com>
2022-08-16 20:34:37 +0700

    added control to read houses

--- controllers/house.js
+++ controllers/house.js
@@ -1,13 +1,30 @@
-const {House} = require('../models')
+const { House, Image, HouseFacility, Facility, Category } = require("../models");
 
 class Controller {
   static async getHouseData(req, res, next) {
     try {
-      const userId = req.user.id;
+      const houses = await House.findAll({
+        attributes: {
+          exclude: ['createdAt', 'updatedAt']
+        },
+        include: [ 
+          {
+            model: Image,
+            attributes: ['id', 'imageUrl']
+          },
+          {
+            model: Category,
+            attributes: ['id', 'name']
+          }
+        ],
+        order: [['name']]
+      });
+
+      res.status(200).json({ houses });
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 }
 
-module.exports = Controller
+module.exports = Controller;
