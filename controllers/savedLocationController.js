95c6ef661468ed2377d1adfb022b15696655ef34
Joshua Christian <jochris1506@gmail.com>
2022-08-16 17:09:38 +0700

    create setup

--- controllers/savedLocationController.js
+++ controllers/savedLocationController.js
@@ -1,6 +1,11 @@
 const { PopularLocation, SavedLocation, User } = require('../models')
 
 class SavedLocationController {
+    static async createSavedLocation(req, res, next) {
+        try {
+        } catch (error) {
+        }
+    }
 }
 
 module.exports = SavedLocationController
--- routers/savedLocation.js
+++ routers/savedLocation.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const SavedLocationController = require('../controllers/savedLocationController')
 
+router.post('/', SavedLocationController.createSavedLocation)
 router.get('/', SavedLocationController)
-router.post('/', SavedLocationController)
+router.get('/:saveId', SavedLocationController)
 router.delete('/:savedId', SavedLocationController)
 
 module.exports = router
