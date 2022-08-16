98d1f0efaa7db633e3bff10d919a1ddc44ffde43
SyahidbinFadlyl <ava.rageman18@gmail.com>
2022-08-16 23:00:09 +0700

    Add login end point and Controller login

--- controller/login.js
+++ controller/login.js
@@ -6,7 +6,51 @@ const makeRandomPassword = require("../helper/makeRandomPw");
 require("dotenv").config();
 
 class Login {
+    static async postLogin(req, res, next) {
+        try {
+            const { email, password } = req.body;
 
+            if (!email && !password) {
+                throw { name: "EmailAndPasswordRequired" };
+            }
+            if (!email) {
+                throw { name: "EmailRequired" };
+            }
+            if (!email.includes("@")) {
+                throw { name: "invalidEmail" };
+            }
+            if (!password) {
+                throw { name: "PasswordRequired" };
+            }
+            const findUser = await User.findOne({
+                where: {
+                    email
+                }
+            });
+            if (!findUser) {
+                throw { name: "InvalidEmailOrPassword" };
+            }
+
+            const passwordValidation = comparePassword(password, findUser.password);
+
+            if (!passwordValidation) {
+                throw { name: "InvalidEmailOrPassword" };
+            }
+
+            const payload = {
+                id: findUser.id,
+            };
+
+            const accessToken = createToken(payload);
+
+            res.status(200).json({
+                access_token: accessToken,
+                email: findUser.email,
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
 }
 
 module.exports = Login;
