5017fb4483dab4623b334674fee17bb8663221b8
rysaldiwijaya <rysaldii@gmail.com>
2022-08-16 20:59:02 +0700

    added read detail house

--- controllers/house.js
+++ controllers/house.js
@@ -5,22 +5,65 @@ class Controller {
     try {
       const houses = await House.findAll({
         attributes: {
-          exclude: ['createdAt', 'updatedAt']
+          exclude: ["createdAt", "updatedAt"],
         },
-        include: [ 
+        include: [
           {
             model: Image,
-            attributes: ['id', 'imageUrl']
+            attributes: ["id", "imageUrl"],
           },
           {
             model: Category,
-            attributes: ['id', 'name']
-          }
+            attributes: ["id", "name"],
+          },
         ],
-        order: [['name']]
+        order: [["name"]],
       });
 
       res.status(200).json({ houses });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getHouseDetail(req, res, next) {
+    try {
+      const { houseId } = req.params;
+      if (isNaN(+houseId)) {
+        throw { name: "Invalid Id" };
+      }
+
+      const house = await House.findByPk(houseId, {
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+        include: [
+          {
+            model: Image,
+            attributes: ["id", "imageUrl"],
+          },
+          {
+            model: Category,
+            attributes: ["id", "name"],
+          },
+          {
+            model: HouseFacility,
+            attributes: ['id'],
+            include: [
+              {
+                model: Facility,
+                attributes: ['name']
+              }
+            ],
+          }
+        ],
+      });
+
+      if (!house) {
+        throw { name: "NotFound" };
+      }
+
+      res.status(200).json(house);
     } catch (error) {
       next(error);
     }
--- routers/houses.js
+++ routers/houses.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const HouseController = require('../controllers/house')
 
 router.get('/', HouseController.getHouseData)
+router.get('/:houseId', HouseController.getHouseDetail)
 
 module.exports = router
