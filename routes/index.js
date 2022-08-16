ef6bf84f16bdd2ef7e539b5adff9d3902b00ba80
yasmirsa <mirsa.raib@gmail.com>
2022-08-16 15:18:17 +0700

    revisi

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const routerUser = require("./user");
 const routerTeacher = require("./teacher");
 const errorHandler = require("../middlewares/errorHandler");
+const CoursesController = require("../controllers/coursecontroller");
 
 router.use("/users", routerUser);
 router.use("/teachers", routerTeacher);
+
+router.get("/courses", CoursesController.fetchCourses);
+router.get("/course/:courseId", CoursesController.fetchCoursesById);
+
 router.use(errorHandler);
