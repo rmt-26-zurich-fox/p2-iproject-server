32a5df388574e0286ccecb972e915bdc3325827a
SyahidbinFadlyl <ava.rageman18@gmail.com>
2022-08-16 23:08:52 +0700

    Add Hook beforeCreate hashPassword & email to lowerCase before create

--- controller/register.js
+++ controller/register.js
@@ -3,7 +3,8 @@ const { User } = require("../models");
 class Register {
     static async postRegister(req, res, next) {
         try {
-            const { email, password } = req.body;
+            let { email, password } = req.body;
+            email = email.toLowerCase();
             const findUser = await User.findOne({ where: { email } });
             if (findUser) {
                 throw { name: "EmailAlreadyExists" };
@@ -14,9 +15,8 @@ class Register {
             });
             res.status(201).json({
                 message: `${email} successfully registered`,
-                user: {
-                    email: email,
-                }
+                email: email,
+
             });
         } catch (error) {
             next(error);
