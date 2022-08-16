be6d062da886cf2f01a8d70bcb1b1dbb334c0232
Joshua Christian <jochris1506@gmail.com>
2022-08-16 18:44:54 +0700

    googlesignin done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const { Category, Product, User } = require('../models')
 const { hashPassword, compareHash } = require('../helpers/bcrypt')
 const { createToken, verifyToken } = require('../helpers/jwt')
+const { OAuth2Client } = require('google-auth-library')
+
 
 class UserController {
     static async createUsers(req, res, next) {
@@ -40,6 +42,38 @@ class UserController {
             const access_token = createToken(payload)
             res.status(200).json({
                 access_token: access_token, id: findUser.id, email: findUser.email, location: findUser.location
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async googleSignInCustomer(req, res, next) {
+        try {
+            const { token_google } = req.headers
+
+            const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
+            const ticket = await client.verifyIdToken({
+                idToken: token_google,
+                audience: process.env.GOOGLE_CLIENT_ID
+            })
+            const payload = ticket.getPayload()
+            const [user, created] = await User.findOrCreate({
+                where: {
+                    email: payload.email,
+                },
+                defaults: {
+                    email: payload.email,
+                    password: "password_google",
+                    phoneNumber: "Google_phone_number",
+                    location: "Jakarta"
+                },
+                hooks: false,
+            })
+
+            const access_token = createToken({ id: user.id })
+            res.status(200).json({
+                access_token: access_token, id: user.id, email: user.email, location: user.location
             })
         } catch (error) {
             next(error)
