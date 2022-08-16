c6d381e0da68b052d957cf55c5e92ce2f3ae6c85
Ghozi Fattah Allauddin <ghozifattah@gmail.com>
2022-08-16 17:44:01 +0700

    Revert "Merge pull request #1 from ghozifa/feat-register-users"
    This reverts commit 945b83ccbf807fa08d2d76f9c0f482040f3ff525, reversing
    changes made to 2f6fc4c14a6778a54ceee5f6e162f2fa17a7ba54.

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 
 const cors = require("cors");
 const express = require("express");
-const app = express();
 const router = require("./routes");
-const errorHandler = require("./middlewares/errorHandler");
+const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
-app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
