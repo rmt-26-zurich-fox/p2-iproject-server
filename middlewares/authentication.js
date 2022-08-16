2f6fc4c14a6778a54ceee5f6e162f2fa17a7ba54
Ghozi Fattah Allauddin <ghozifattah@gmail.com>
2022-08-16 14:14:23 +0700

    feat: setup server

---
+++ middlewares/authentication.js
@@ -0,0 +1,21 @@
+const { verifyToken } = require("../helper/jwt");
+const { User } = require("../models");
+
+const authentication = async (req, res, next) => {
+    try {
+        const { access_token } = req.headers;
+        if (!access_token) throw { name: "nullToken" }
+        const payload = verifyToken(access_token);
+        const user = await User.findByPk(payload.id);
+        if (!user) throw { name: "unauthorized" }
+        req.user = {
+            id: user.id,
+            role: user.role
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = authentication;
