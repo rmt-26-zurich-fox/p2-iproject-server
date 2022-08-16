89fd3a80e9d23a8eadd729e6899613ac26ad3f9d
Ghozi Fattah Allauddin <ghozifattah@gmail.com>
2022-08-16 17:44:14 +0700

    Revert "Revert "Merge pull request #1 from ghozifa/feat-register-users""
    This reverts commit c6d381e0da68b052d957cf55c5e92ce2f3ae6c85.

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 
 const cors = require("cors");
 const express = require("express");
-const router = require("./routes");
 const app = express();
+const router = require("./routes");
+const errorHandler = require("./middlewares/errorHandler");
 const port = 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,10 @@
+const UserController = require("../controller/userController");
 const router = require("express").Router();
 
 // Admin
+router.post("/register/admin", UserController.registerAdmin);
 
 // Visitor
+router.post("/register", UserController.registerVisitor);
 
 module.exports = router;
