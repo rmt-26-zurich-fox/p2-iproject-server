42c7918a98c5abebf4de56ed0f8547054054fcb2
yasmirsa <mirsa.raib@gmail.com>
2022-08-17 14:09:30 +0700

    add fetch shopping cart

--- routes/student.js
+++ routes/student.js
@@ -2,5 +2,6 @@ const express = require("express");
 const StudentController = require("../controllers/studentcontroller");
 const router = express.Router();
 
+router.post("/shopping-cart", StudentController.fetchShoppingCart);
 router.post("/shopping-cart/:courseId", StudentController.addShoppingCart);
 module.exports = router;
