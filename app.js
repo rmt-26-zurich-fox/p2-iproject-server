869178c2f4a00e9f6f0f3fa580643502e2820bef
erlanggakpp <erlanggakencana@gmail.com>
2022-08-18 01:17:45 +0700

    Added live-chat feature

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const router = require("./routes/index");
 const cors = require("cors");
 
 // SOCKET
-const { createServer } = require("http");
+const http = require("http");
+const server = http.createServer(app);
 const { Server } = require("socket.io");
-const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer, {
+// const io = new Server(server);
+
+const io = require("socket.io")(server, {
   cors: {
-    origins: ["http://127.0.0.1:5173"],
+    origins: ["http://127.0.0.1:8080"],
   },
 });
 
@@ -23,46 +25,46 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(router);
+
+// app.listen(port, () => {
+//   console.log(`App is listening to ${port}`);
+// });
+let messages = [];
+let users = [];
+
+io.on("connection", (socket) => {
+  socket.emit("init-chat", messages);
+  socket.emit("update-users", users);
+  socket.on("send-msg", function (data) {
+    var newMessage = {
+      text: data.message,
+      user: data.user,
+    };
+    messages.push(newMessage);
+    io.emit("read-msg", newMessage);
+  });
+
+  socket.on("add-user", function (user) {
+    console.log(user);
+    users.push({ id: socket.id, name: user });
+    console.log(users);
+    io.emit("update-users", users);
+  });
+
+  // When user disconnects the server updates user list and emits an event
+  socket.on("disconnect", function () {
+    users = users.filter(function (user) {
+      return user.id != socket.id;
+    });
+    io.emit("update-users", users);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 app.get("/socket-chat", (req, res) => {
   console.log("MASUK SINI");
 });
-
-app.listen(port, () => {
-  console.log(`App is listening to ${port}`);
+server.listen(port, () => {
+  console.log("listening on *:3000");
 });
-// let messages = [];
-// let users = [];
-
-// io.on("connection", (socket) => {
-//   socket.emit("init-chat", messages);
-//   socket.emit("update-users", users);
-//   socket.on("send-msg", function (data) {
-//     var newMessage = {
-//       text: data.message,
-//       user: data.user,
-//       date: dateFormat(new Date(), "shortTime"),
-//     };
-//     messages.push(newMessage);
-//     io.emit("read-msg", newMessage);
-//   });
-
-//   socket.on("add-user", function (user) {
-//     users.push({ id: socket.id, name: user });
-//     io.emit("update-users", users);
-//   });
-
-//   // When user disconnects the server updates user list and emits an event
-//   socket.on("disconnect", function () {
-//     users = users.filter(function (user) {
-//       return user.id != socket.id;
-//     });
-//     io.emit("update-users", users);
-//   });
-//   socket.on("disconnect", () => {
-//     console.log("user disconnected");
-//   });
-// });
-
-// httpServer.listen(port, () => {
-//   console.log("listening on *:3000");
-// });
