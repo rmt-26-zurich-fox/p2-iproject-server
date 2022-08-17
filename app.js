ddaf8ae7425681763707c0507b6496b207d85c2b
Ahmadabd3004 <ahmadazis3004@gmail.com>
2022-08-17 17:14:03 +0700

    getAgents

--- app.js
+++ app.js
@@ -3,16 +3,52 @@ const app = express();
 const port = 3000;
 const axios = require("axios");
 const { Strategy } = require("./models");
+const cors = require("cors");
 
-app.get("/", async (req, res, next) => {
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/agents", async (req, res, next) => {
   try {
     const { data } = await axios.get(
       "https://valorant-api.com/v1/agents?isPlayableCharacter=true&language=id-ID"
     );
-    const nameAgents = data.data.map((e) => {
-      return e.displayName, e.description;
+    const agents = data.data.map((e) => {
+      return {
+        name: e.displayName,
+        description: e.description,
+        icon: e.displayIcon,
+        img: e.fullPortrait,
+        role: {
+          name: e.role.displayName,
+          image: e.role.displayIcon,
+        },
+        abilities: {
+          ability1: {
+            name: e.abilities[0].displayName,
+            description: e.abilities[0].description,
+            imgAbility: e.abilities[0].displayIcon,
+          },
+          ability2: {
+            name: e.abilities[1].displayName,
+            description: e.abilities[1].description,
+            imgAbility: e.abilities[1].displayIcon,
+          },
+          ability3: {
+            name: e.abilities[2].displayName,
+            description: e.abilities[2].description,
+            imgAbility: e.abilities[2].displayIcon,
+          },
+          ultimate: {
+            name: e.abilities[3].displayName,
+            description: e.abilities[3].description,
+            imgAbility: e.abilities[3].displayIcon,
+          },
+        },
+      };
     });
-    console.log(nameAgents);
+    res.status(200).json(agents);
     // console.log(data.data[0].displayName);
   } catch (error) {
     console.log(error);
