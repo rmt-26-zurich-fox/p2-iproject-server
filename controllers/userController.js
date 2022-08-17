48dbdb6ddd7eab5d3bcad51cea0dabba7ec28620
erlanggakpp <erlanggakencana@gmail.com>
2022-08-17 22:39:04 +0700

    Added google-signin feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const { hashPassword, compareHashPassword } = require("../helpers/bcrypt");
 const { createToken } = require("../helpers/jwt");
 const { User } = require("../models");
+const { OAuth2Client } = require("google-auth-library");
 
 class UserController {
   static async register(req, res, next) {
@@ -55,6 +56,41 @@ class UserController {
       const token = createToken(payload);
       res.status(200).json({ access_token: token });
     } catch (error) {
+      next(error);
+    }
+  }
+
+  static async googleSignIn(req, res, next) {
+    try {
+      const { google_token } = req.headers;
+      const client = new OAuth2Client(process.env.CLIENT_ID);
+      const ticket = await client.verifyIdToken({
+        idToken: google_token,
+        audience: process.env.CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+      });
+      const payload = ticket.getPayload();
+      console.log(payload);
+      const { email, name } = payload;
+      const [user, created] = await User.findOrCreate({
+        where: { email: email },
+        defaults: {
+          email: email,
+          password: "Your password",
+          role: "Visitor",
+        },
+        hooks: false,
+      });
+      const access_token = createToken({
+        id: user.id,
+      });
+      res.status(200).json({ access_token: `${access_token}`, user: user });
+      // console.log(created, "INI status user");
+      // If request specified a G Suite domain:
+      // const domain = payload['hd'];
+    } catch (error) {
+      console.log(error);
       next(error);
     }
   }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,4 +3,5 @@ const UserController = require("../controllers/userController");
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
+router.post("/google-signin", UserController.googleSignIn);
 module.exports = router;
