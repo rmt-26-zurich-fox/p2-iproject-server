ec520b57bd1398af000943247ecd2d8e52f0bdd7
naamalia23 <asmi.amalia23@gmail.com>
2022-08-17 10:52:26 +0700

    show all product feature

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,5 +1,22 @@
+const { Product } = require("../models");
+
 class ProductController {
-    
+  static async showAllProduct(req, res, next) {
+    try {
+      let products = await Product.findAll({
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+      });
+      res.status(200).json({
+        message: "success read all products",
+        products,
+      });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
 }
 
 module.exports = ProductController;
--- routers/productRouter.js
+++ routers/productRouter.js
@@ -2,8 +2,6 @@ const express = require("express");
 const productRouter = express.Router();
 const ProductController = require("../controllers/ProductController");
 
-productRouter.get("/", async (req, res) => {
-  res.send("hello dari product");
-});
+productRouter.get("/", ProductController.showAllProduct);
 
 module.exports = productRouter;
