b594b683a9c190fbcd033843c1f8917b0c0036b6
SyahidbinFadlyl <ava.rageman18@gmail.com>
2022-08-17 22:38:32 +0700

    Add User seeder

---
+++ seeders/20220817153316-User_seeder.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const { hashPassword } = require("../helper/bcrypt");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert("User", [{
+      username: "Tanaka Ryuunosuke",
+      email: "tanaka.ryu18@gmail.com",
+      password: hashPassword("tanaka"),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("User");
+  }
+};
