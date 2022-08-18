081c3acfa6ac4302ed4caa7102831d7435c0e50d
inyund <mfrafie@gmail.com>
2022-08-18 07:22:57 +0700

    setup socket

--- bin/www.js
+++ bin/www.js
@@ -1,10 +1,20 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-
 const app = require("../app");
-const port = process.env.PORT || 3000;
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Express server listening on port ${server.address().port}`);
+});
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-app.listen(port, () => {
-  console.log("Listening to port", port);
+io.on("connection", (socket) => {
+  socket.on("message", (data) => {
+    console.log("user sending message with: ", data);
+  });
+  console.log("a user connected");
 });
