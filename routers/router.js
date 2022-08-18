83397a5c1e534c13e4abcf1582b60d9363de4f7a
Rio Hanantyoko <rhanantyoko@gmail.com>
2022-08-18 22:29:16 +0700

    discord oauth return access_token, fucking finally

--- routers/router.js
+++ routers/router.js
@@ -1,51 +1,86 @@
-const express = require('express')
-// const passport = require('passport')
-// const Strategy = require('passport-discord/lib/strategy')
-const productController = require('../controllers/productController')
-const userController = require('../controllers/userController')
-const authentication = require('../middlewares/authentication')
-const authorization = require('../middlewares/authorization')
+const express = require("express");
+const passport = require("passport");
+const Strategy = require("passport-discord/lib/strategy");
+const productController = require("../controllers/productController");
+const userController = require("../controllers/userController");
+const authentication = require("../middlewares/authentication");
+const authorization = require("../middlewares/authorization");
+const axios = require("axios");
+const queryString = require("querystring");
 
+const btoa = require("btoa");
 
-
-const router = express.Router()
-
-
-router.post('/admin/register', userController.adminRegister)
-router.post('/admin/login', userController.adminLogin)
-router.post('/cust/register', userController.registerCustomer )
-router.post('/cust/login', userController.loginCustomer )
-router.post('/cust/googleSignIn', userController.googleLoginCustomer)
-
-// router.get('/api/auth/discord/', passport.authenticate('discord'))
-// router.get('/api/auth/discord/redirect', passport.authenticate('discord'), (req,res)=>{
-//     let {code} = req.query
+// async function exchangeCode(code){
 //     console.log(code);
-//     router.post('https://discord.com/api/oauth2/token', (req,res)=>{
-//         const data={
-//             'client_id' : process.env.DISCORD_CLIENT_ID,
-//             'client_secret' : process.env.DISCORD_CLIENT_SECRET,
-//             'grant_type': 'authorization_code',
-//             'code' : code,
-//             'redirect_uri': process.env.DISCORD_CLIENT_REDIRECT      
-//         }
-//         console.log(res);
-//     })
-    
-// })
-router.get('/products', productController.getProducts)
-router.get('/products/:productId', productController.getOneProduct)
+//     try {
+//         const oauth = await axios.post('https://discord.com/api/oauth2/token', queryString.stringify(
 
-router.use(authentication)
+//             {
+//                 'client_id' : process.env.DISCORD_CLIENT_ID,
+//                 'client_secret' : process.env.DISCORD_CLIENT_SECRET,
+//                 'grant_type': "authorization_code",
+//                 'code' : code,
+//                 'redirect_uri': process.env.DISCORD_CLIENT_REDIRECT
+//             }),
+//             {
+//             'Content-Type': 'application/x-www-form-urlencoded'
+//            }
+//        )
+//        console.log(oauth);
+//         return oauth
+//     } catch (error) {
+//         console.log(error);
+//     }
 
-router.post('/products/add',authorization, productController.createProduct)
-router.get('/cart', productController.getCart)
-router.delete('/cart', productController.emptyCart)
-router.get('/cart/total', productController.getTotalPrice)
-router.delete('/products/:productId/delete', authorization, productController.deleteProduct)
-router.post('/cart/:productId', productController.addtoCart)
-router.delete('/cart/:cartId', productController.deleteItemFromCart)
+// }
 
+const router = express.Router();
 
+router.post("/admin/register", userController.adminRegister);
+router.post("/admin/login", userController.adminLogin);
+router.post("/cust/register", userController.registerCustomer);
+router.post("/cust/login", userController.loginCustomer);
+router.post("/cust/googleSignIn", userController.googleLoginCustomer);
 
-module.exports=router
+router.get("/api/auth/discord/", passport.authenticate("discord"));
+router.get("/api/auth/discord/redirect", async (req, res) => {
+  try {
+    let { code } = req.query;
+    const oauth = await axios.post(
+      "https://discord.com/api/oauth2/token",
+      queryString.stringify({
+        client_id: process.env.DISCORD_CLIENT_ID,
+        client_secret: process.env.DISCORD_CLIENT_SECRET,
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: process.env.DISCORD_CLIENT_REDIRECT,
+      }),
+      {
+        "Content-Type": "application/x-www-form-urlencoded",
+      }
+    );
+    const {access_token} = oauth.data
+    res.status(201).json(access_token);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/products", productController.getProducts);
+router.get("/products/:productId", productController.getOneProduct);
+
+router.use(authentication);
+
+router.post("/products/add", authorization, productController.createProduct);
+router.get("/cart", productController.getCart);
+router.delete("/cart", productController.emptyCart);
+router.get("/cart/total", productController.getTotalPrice);
+router.delete(
+  "/products/:productId/delete",
+  authorization,
+  productController.deleteProduct
+);
+router.post("/cart/:productId", productController.addtoCart);
+router.delete("/cart/:cartId", productController.deleteItemFromCart);
+
+module.exports = router;
