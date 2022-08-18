b7834d6565cf112ed227cc37d380f469890c2d96
yasmirsa <mirsa.raib@gmail.com>
2022-08-18 22:56:38 +0700

    add Midtrans

--- routes/student.js
+++ routes/student.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const InvoiceController = require("../controllers/invoicecontroller");
 const StudentController = require("../controllers/studentcontroller");
 const router = express.Router();
 
 router.post("/shopping-cart", StudentController.fetchShoppingCart);
 router.post("/shopping-cart/:courseId", StudentController.addShoppingCart);
+router.get("/checkout", InvoiceController.checkout);
 module.exports = router;
