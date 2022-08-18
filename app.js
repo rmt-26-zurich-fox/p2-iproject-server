007ef57d2969ab58998cccc170804f86daa74638
yasmirsa <mirsa.raib@gmail.com>
2022-08-18 12:20:19 +0700

    revisi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const routerIndex = require("./routes/index");
 
 app.use(cors());
