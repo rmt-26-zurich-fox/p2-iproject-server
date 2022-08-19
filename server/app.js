2e69349def9f807ab45566b44ba1fdbd5ab62cda
HafizhIslami <hafizhislami11@gmail.com>
2022-08-19 09:15:50 +0700

    edit cors

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = require("./routes");
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: 'https://otohelper1.web.app'}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
