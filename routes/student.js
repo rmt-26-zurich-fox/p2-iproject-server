18d9cb25e1fec7c8112cc970ae43c8d5bab0bce0
yasmirsa <mirsa.raib@gmail.com>
2022-08-19 08:47:10 +0700

    unfinished google calendar

--- routes/student.js
+++ routes/student.js
@@ -3,7 +3,9 @@ const InvoiceController = require("../controllers/invoicecontroller");
 const StudentController = require("../controllers/studentcontroller");
 const router = express.Router();
 
-router.post("/shopping-cart", StudentController.fetchShoppingCart);
+router.get("/shopping-cart", StudentController.fetchShoppingCart);
+router.post("/courselist/add", StudentController.addToCourseList);
 router.post("/shopping-cart/:courseId", StudentController.addShoppingCart);
+router.delete("/shopping-cart/:courseId/delete");
 router.get("/checkout", InvoiceController.checkout);
 module.exports = router;
