1a3d636d0fbd0680ffaacaf63ad29a15bc701fcb
HafizhIslami <hafizhislami11@gmail.com>
2022-08-19 09:23:01 +0700

    update cors

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = require("./routes");
 const app = express();
 
-app.use(cors({origin: '*'}));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -64,6 +64,16 @@ class PaymentController {
       .createTransaction(parameter)
       .then((transaction) => {
         // transaction token
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader(
+          "Access-Control-Allow-Methods",
+          "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+        ); // If needed
+        res.setHeader(
+          "Access-Control-Allow-Headers",
+          "X-Requested-With,content-type"
+        ); // If needed
+        res.setHeader("Access-Control-Allow-Credentials", true); // If needed
         res.status(201).json(transaction);
         let transactionToken = transaction.token;
         console.log("transactionToken:", transactionToken);
