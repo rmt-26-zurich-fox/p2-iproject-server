2e83fe0f6f653dec1db3373a3928168384d6b4a7
HafizhIslami <hafizhislami11@gmail.com>
2022-08-19 09:59:58 +0700

    9.59

--- server/routes/request.js
+++ server/routes/request.js
@@ -1,13 +1,14 @@
 const requestRouter = require("express").Router();
 const PaymentController = require("../controllers/paymentController");
 const RequestController = require("../controllers/requestController");
+const allowAccess = require("../middleware/access");
 
 requestRouter.get("/", RequestController.fetchAllRequest);
 requestRouter.post("/product/:ProductId", RequestController.addProduct);
 requestRouter.post("/service/:ServiceId", RequestController.addService);
 requestRouter.delete("/product/:ProductId", RequestController.cancelProduct);
 requestRouter.delete("/service/:ServiceId", RequestController.cancelService);
-requestRouter.get("/payment", PaymentController.getBill);
+requestRouter.get("/payment", allowAccess, PaymentController.getBill);
 requestRouter.post("/payment/status", PaymentController.changeStatus);
 
 module.exports = requestRouter;
